eaadf26235daea514035306136781e82e1597a3a
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-03-29 22:58:52 +0900

    Fix: Singleton pattern

--- design_pattern/Singleton Pattern/Singleton_2.js
+++ design_pattern/Singleton Pattern/Singleton_2.js
@@ -27,3 +27,6 @@ class Counter {
 //Object.freeze: 객체를 사용하는 쪽에서 직접 객체를 수정할 수 없도록 세팅 -> Singleton인스턴스이 프로퍼티 덮어쓰는 실수 예방
 const singletonCounter = Object.freeze(new Counter());
 export default singletonCounter;
+
+console.log(object);
+singletonCounter.decrement();
